4765aa60aac858496e94304cbeb83e2d3d8afbe1
andres.villegas <andresvillegass@gmail.com>
2019-03-23 20:28:30 -0500

    creando mi primera pagina

--- express.js
+++ express.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
- 
-app.get('/', function (req, res) {
+
+app.use(express.static(__dirname+'/public'))
+/*app.get('/', function (req, res) {
   res.send('Hello World')
-})
+})*/
  
 app.listen(3000)
